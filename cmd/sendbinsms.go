75b61d0f372f90aed151e9820408508d3544be01
Jakub Oboza <jakub.oboza@gmail.com>
2022-10-08 18:30:27 +0200

    added sendbinsms and sendwappush command line interface tools

--- cmd/sendbinsms.go
+++ cmd/sendbinsms.go
@@ -4,36 +4,98 @@ Copyright © 2022 Jakub Oboza <jakub.oboza@gmail.com>
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
 
+	"github.com/JakubOboza/fonix/client"
 	"github.com/spf13/cobra"
 )
 
 // sendbinsmsCmd represents the sendbinsms command
 var sendbinsmsCmd = &cobra.Command{
 	Use:   "sendbinsms",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "sendbinsms sends binary configured sms via fonix api",
+	Long:  `endbinsms sends binary configured sms via fonix api`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sendbinsms called")
+		apiKey := os.Getenv("API_KEY")
+
+		flagApiKey, err := cmd.Flags().GetString("url")
+		if err == nil && flagApiKey != "" {
+			apiKey = flagApiKey
+		}
+
+		// params
+
+		originator, err := cmd.Flags().GetString("originator")
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		binBody, err := cmd.Flags().GetString("binbody")
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		numbers, err := cmd.Flags().GetString("numbers")
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		dummy, err := cmd.Flags().GetString("dummy")
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		params := &client.SmsBinParams{
+			Originator: originator,
+			BinBody:    binBody,
+			Numbers:    numbers,
+			Dummy:      dummy,
+		}
+
+		fonixClient := client.New(apiKey)
+
+		newBaseUrl, _ := cmd.Flags().GetString("baseurl")
+		if newBaseUrl != "" {
+			fonixClient.SetBaseURL(newBaseUrl)
+		}
+
+		result, err := fonixClient.SendBinSms(context.Background(), params)
+
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		fmt.Println("======Success======")
+		fmt.Println("Guid: ", result.TxGuid)
+		fmt.Println("Numbers: ", result.Numbers)
+		fmt.Println("Parts: ", result.SmsParts)
+		fmt.Println("Encoding: ", result.Encoding)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(sendbinsmsCmd)
 
-	// Here you will define your flags and configuration settings.
+	sendbinsmsCmd.Flags().StringP("apikey", "k", "", "apikey for the service eg: --apikey=live:myKey123456XYZ")
+	sendbinsmsCmd.Flags().StringP("binbody", "b", "", "body of the text message eg: --binbody=C024A3A5905E195B081180800991C6106DA620420620A22C4986166184289B526204")
+	sendbinsmsCmd.Flags().StringP("originator", "o", "", "originator of the message eg: --originator=889988")
+	sendbinsmsCmd.Flags().StringP("numbers", "n", "", "numbers to send the sms to eg: --numbers=4474123456778")
+	sendbinsmsCmd.Flags().StringP("dummy", "d", "", "dummy flag yes or no, you can skip it. dummy=yes will make fonix not send sms but mock respond eg: --dummy=yes")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// sendbinsmsCmd.PersistentFlags().String("foo", "", "A help for foo")
+	sendbinsmsCmd.MarkFlagRequired("binbody")
+	sendbinsmsCmd.MarkFlagRequired("originator")
+	sendbinsmsCmd.MarkFlagRequired("numbers")
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// sendbinsmsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	sendbinsmsCmd.Flags().StringP("baseurl", "u", "", "base url of fonix/sonar that isnt default. --baseurl=https://sonar.fonix.io")
 }
--- cmd/sendwappush.go
+++ cmd/sendwappush.go
@@ -4,36 +4,107 @@ Copyright © 2022 Jakub Oboza <jakub.oboza@gmail.com>
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
 
+	"github.com/JakubOboza/fonix/client"
 	"github.com/spf13/cobra"
 )
 
 // sendwappushCmd represents the sendwappush command
 var sendwappushCmd = &cobra.Command{
 	Use:   "sendwappush",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "sendwappush sends wap api push via fonix api",
+	Long:  `command line tool to send wap push via fonix gateway v2 api`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sendwappush called")
+		apiKey := os.Getenv("API_KEY")
+
+		flagApiKey, err := cmd.Flags().GetString("url")
+		if err == nil && flagApiKey != "" {
+			apiKey = flagApiKey
+		}
+
+		// params
+
+		originator, err := cmd.Flags().GetString("originator")
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		pushTitle, err := cmd.Flags().GetString("title")
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		pushLink, err := cmd.Flags().GetString("link")
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		numbers, err := cmd.Flags().GetString("numbers")
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		dummy, err := cmd.Flags().GetString("dummy")
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		params := &client.SmsWapParams{
+			Originator: originator,
+			PushTitle:  pushTitle,
+			PushLink:   pushLink,
+			Numbers:    numbers,
+			Dummy:      dummy,
+		}
+
+		fonixClient := client.New(apiKey)
+
+		newBaseUrl, _ := cmd.Flags().GetString("baseurl")
+		if newBaseUrl != "" {
+			fonixClient.SetBaseURL(newBaseUrl)
+		}
+
+		result, err := fonixClient.SendWapPush(context.Background(), params)
+
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		fmt.Println("======Success======")
+		fmt.Println("Guid: ", result.TxGuid)
+		fmt.Println("Numbers: ", result.Numbers)
+		fmt.Println("Parts: ", result.SmsParts)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(sendwappushCmd)
 
-	// Here you will define your flags and configuration settings.
+	sendwappushCmd.Flags().StringP("apikey", "k", "", "apikey for the service eg: --apikey=live:myKey123456XYZ")
+	sendwappushCmd.Flags().StringP("title", "t", "", "push title eg: --title=hello")
+	sendwappushCmd.Flags().StringP("link", "l", "", "push link eg: --link=http://google.com/link/to/push")
+	sendwappushCmd.Flags().StringP("originator", "o", "", "originator of the message eg: --originator=889988")
+	sendwappushCmd.Flags().StringP("numbers", "n", "", "numbers to send the sms to eg: --numbers=4474123456778")
+	sendwappushCmd.Flags().StringP("dummy", "d", "", "dummy flag yes or no, you can skip it. dummy=yes will make fonix not send sms but mock respond eg: --dummy=yes")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// sendwappushCmd.PersistentFlags().String("foo", "", "A help for foo")
+	sendwappushCmd.MarkFlagRequired("title")
+	sendwappushCmd.MarkFlagRequired("link")
+	sendwappushCmd.MarkFlagRequired("originator")
+	sendwappushCmd.MarkFlagRequired("numbers")
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// sendwappushCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	sendwappushCmd.Flags().StringP("baseurl", "u", "", "base url of fonix/sonar that isnt default. --baseurl=https://sonar.fonix.io")
 }
