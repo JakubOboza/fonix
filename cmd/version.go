d360b684fd4a97b07d8ab642b0d26512d6301164
Jakub Oboza <jakub.oboza@gmail.com>
2022-11-27 10:26:39 +0100

    added client version for command line client

---
+++ cmd/version.go
@@ -0,0 +1,25 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/JakubOboza/fonix/client"
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "client version",
+	Long:  `client version. This shows the CLI/Lib client version.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(client.VERSION)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
